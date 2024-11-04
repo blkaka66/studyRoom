41227790b4151af1cddd10e63d297902e903e517
blkaka66 <103988926+blkaka66@users.noreply.github.com>
2024-11-04 13:29:30 +0900

    1104

---
+++ src/main/java/CorsConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080") // 허용할 Origin 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
